4f42c316ce2cb497529f11222a0b4398a0b35df8
lhy <lhy@lhy.com>
2018-06-11 10:07:41 +0800

    翻转字符串

---
+++ reverseString.py
@@ -0,0 +1,17 @@
+"""
+翻转字符串
+
+1.循环赋值
+2.栈
+3.逆序遍历
+
+"""
+
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
